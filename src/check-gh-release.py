511898fd314cc8ac653502816fbde8ac0081e85d
K.Takata <kentkt@csc.jp>
2016-01-11 13:01:53 +0900

    appveyor: Don't skip x86 build

--- src/check-gh-release.py
+++ src/check-gh-release.py
@@ -13,6 +13,7 @@ from datetime import datetime, timedelta
 
 default_repo_name = 'vim/vim'
 repo_name = os.getenv('APPVEYOR_REPO_NAME', default_repo_name)
+repo_tag_name = os.getenv('APPVEYOR_REPO_TAG_NAME', '<dummy>')
 
 gh_release_url = 'https://api.github.com/repos/' + repo_name + '/releases/latest'
 
@@ -24,6 +25,10 @@ except urllib.error.HTTPError:
 
 rel_info = json.load(StringIO(str(response.read(), 'utf-8')))
 print('Last release: ' + rel_info['created_at'])
+
+if rel_info['name'] == repo_tag_name:
+    # Maybe the second pass of the same build.
+    exit(0)
 
 rel_time = datetime.strptime(rel_info['created_at'], '%Y-%m-%dT%H:%M:%SZ')
 if datetime.utcnow() > rel_time + timedelta(days=1):
