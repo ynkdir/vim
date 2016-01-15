9bb8ad243d7158dd32ffa39cbef1a0ee6c922abe
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2016-01-15 20:08:06 +0900

    no comment

--- src/appveyor.bat
+++ src/appveyor.bat
@@ -188,12 +188,56 @@ goto :eof
 ml64 /c dbg_asm.asm
 cl dbg_main.c /I"C:\Program Files\Racket\include" dbg_asm.obj
 .\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
+.\dbg_main.exe
 
 :: Remove progress bar from the build log
 sed -e "s/\$(LINKARGS2)/\$(LINKARGS2) | sed -e 's#.*\\\\r.*##'/" Make_mvc.mak > Make_mvc2.mak
 :: Build GUI version
 nmake -f Make_mvc2.mak CPU=AMD64 ^
-	GUI=yes  ^
+	GUI=yes OLE=yes DIRECTX=yes ^
+	FEATURES=HUGE IME=yes MBYTE=yes ICONV=yes DEBUG=no ^
+	PERL_VER=522 DYNAMIC_PERL=yes PERL=C:\Perl522\perl ^
+	PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27-x64 ^
+	PYTHON3_VER=34 DYNAMIC_PYTHON3=yes PYTHON3=C:\Python34-x64 ^
+	LUA_VER=53 DYNAMIC_LUA=yes LUA=C:\Lua ^
+	TCL_VER=86 TCL_VER_LONG=8.6 DYNAMIC_TCL=yes TCL=C:\Tcl ^
+	RUBY=C:\Ruby22-x64 DYNAMIC_RUBY=yes RUBY_VER=22 RUBY_VER_LONG=2.2.0 ^
+	RUBY_MSVCRT_NAME=msvcrt ^
 	"MZSCHEME=C:\Program Files\Racket" DYNAMIC_MZSCHEME=yes MZSCHEME_VER=3m_9z0ds0 ^
 	WINVER=0x500 ^
 	|| exit 1
